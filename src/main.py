d923c51a083943799f04f412ea06d8a029d808c4
Bhargav Sarvaria <34936854+bhargav-sarvaria@users.noreply.github.com>
2022-03-10 18:33:09 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -6,8 +6,8 @@ import sys
 crawler = Crawler(1)
 if __name__ == '__main__':
     try:
-        # display = Display(visible=0, size=(1024, 768))
-        # display.start()
+        display = Display(visible=0, size=(1024, 768))
+        display.start()
         crawler.crawlUrlsFromConfigPath(sys.argv[1])
         os.environ["HEADLESS"] = sys.argv[2]
         
