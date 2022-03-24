07dfcd7263285b0791b338e99f191b86fe734e9a
Bhargav Sarvaria <34936854+bhargav-sarvaria@users.noreply.github.com>
2022-03-24 11:08:21 +0530

    Chromium

--- config/setup_chrome.sh
+++ config/setup_chrome.sh
@@ -15,11 +15,12 @@ apt-get install -y python3-testresources
 apt install cron
 systemctl enable cron
 
-apt install -y curl
-curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add 
-sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
-apt -y update 
-apt -y install google-chrome-stable 
+# apt install -y curl
+# curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add 
+# sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+# apt -y update 
+# apt -y install google-chrome-stable
+sudo apt install -y chromium-browser 
 
 apt install -y python3-pip
 pip install --upgrade requests
