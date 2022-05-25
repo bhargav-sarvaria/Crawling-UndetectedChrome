2c39841cadf2ded8d9683718c90f203742e96cec
Bhargav Sarvaria <34936854+bhargav-sarvaria@users.noreply.github.com>
2022-05-26 01:02:51 +0530

    Chrome back to 98

--- config/setup_chrome.sh
+++ config/setup_chrome.sh
@@ -18,8 +18,9 @@ systemctl enable cron
 apt install -y curl
 curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add 
 sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
-apt -y update 
-apt -y install google-chrome-stable
+wget --no-verbose -O /tmp/chrome.deb https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_98.0.4758.102-1_amd64.deb 
+apt install -y /tmp/chrome.deb 
+rm /tmp/chrome.deb
 
 apt install -y python3-pip
 pip install --upgrade requests
